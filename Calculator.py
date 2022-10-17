c5914fe6a4c2f45809c568ff10a727c1490f5bca
Saadiq's Dev <96834576+Mrsupertag@users.noreply.github.com>
2022-10-17 20:57:24 +0600

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -5,7 +5,7 @@ multi = "*"
 div = "/" 
 user_input = input("Enter type: ")
 user_input2 = input("Enter number 1: ")
-user_input3 = input("Enter number 2: )
+user_input3 = input("Enter number 2: ")
 
 if user_input == add:
   print(user_input2)
