b28bfc12327c84767bc006441140d4f4c418e041
Saadiq's Dev <96834576+Mrsupertag@users.noreply.github.com>
2022-10-17 20:56:30 +0600

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,28 @@
+print ("Welcome!!")
+add = "+"
+sub = "-"
+multi = "*"
+div = "/" 
+user_input = input("Enter type: ")
+user_input2 = input("Enter number 1: ")
+user_input3 = input("Enter number 2: )
+
+if user_input == add:
+  print(user_input2)
+  print(user_input3)
+  print(user_input2 + user_input3)
+  
+if user_input == sub:
+  print(user_input2)
+  print(user_input3)
+  print(user_input2 - user_input3)
+  
+if user_input == multi:
+  print(user_input2)
+  print(user_input3)
+  print(user_input2 * user_input3)
+  
+ if user_input == div:
+   print(user_input2)
+   print(user_input3)
+   print(user_input2 / user_input3)
