21bac2878750fa13a9328c41113924b7fcc9f424
Saadiq's Dev <96834576+Mrsupertag@users.noreply.github.com>
2022-10-18 21:10:50 +0600

    Add files via upload

---
+++ Chat.py
@@ -0,0 +1,22 @@
+print("Hello There!")
+user_input = input("Say something! ")
+hello = "hello"
+good = "I am good!"
+bad = "I am not good"
+music = "I like to listen to music"
+print(user_input)
+
+if user_input == hello:
+  print("How are you?")
+ else:
+   print("How are you?")
+
+if user_input == good:
+  print("Nice! I am good!")
+if user_input == bad:
+  print("Oh i see , i m doing good!")
+
+if user_input == music:
+  print("Music is everything!")
+else:
+  print("Thats nice!")
