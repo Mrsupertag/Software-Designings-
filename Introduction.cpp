2759a21c3bb90fc06ad3e6467ceedc4a082a406b
Saadiq's Dev <96834576+Mrsupertag@users.noreply.github.com>
2022-10-18 21:25:00 +0600

    Update and rename Test.cpp to Introduction.cpp

---
+++ Introduction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+ int age = 13;
+string name = "Saadiq";
+ int mass = 40;
+ char grade = 'B';
+
+ cout << "Your name is " << name << "\n";
+ cout << "Your age is " << age << "\n";
+ cout << "Your mass is " << mass << "\n";
+ cout << "Your normal grade is " << grade;
+ 
+ 
+ return 0;
+  } 
