135c84b063b33423e4db36e1bff2998c8f677eb1
Saadiq's Dev <96834576+Mrsupertag@users.noreply.github.com>
2022-10-18 21:24:06 +0600

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+ int age = 13;
+ name = "Saadiq";
+
+ cout << "Your name is " << name << "\n";
+ cout << "Your age is " << age;
+ 
+ return 0;
+}
