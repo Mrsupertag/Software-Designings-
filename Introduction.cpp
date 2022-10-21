d901cbcdc47b217bdab29788f630069400426053
Saadiq's Dev <96834576+Mrsupertag@users.noreply.github.com>
2022-10-21 14:26:53 +0600

    Update Introduction.cpp

--- Introduction.cpp
+++ Introduction.cpp
@@ -3,15 +3,23 @@ using namespace std;
 int main() {
   
  int age = 13;
-string name = "Saadiq";
  int mass = 40;
+ string name = "Saadiq";
  char grade = 'B';
+ int role = 3;
+ string country = "Bangladesh";
+ string city = "Dhaka";
+ string area = "Mogbazar";
+ string school = "St Mary's Inter School";
 
- cout << "Your name is " << name << "\n";
- cout << "Your age is " << age << "\n";
- cout << "Your mass is " << mass << "\n";
- cout << "Your normal grade is " << grade;
- 
+ cout << "My name is " << name << "\n";
+ cout << "My age is " << age << "\n";
+ cout << "My mass is " << mass << "\n";
+ cout << "My minimum grade is " << grade << "\n";
+ cout << "I live in " << country << "\n";
+ cout << "I live in " << city << " " << "at " << area << "\n";
+ cout << "I study at " << school << "\n";
+ cout << "Have a nice day!";
  
  return 0;
-  } 
+}
