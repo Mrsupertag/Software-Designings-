2824abbd185ae97470d9a215d83ca39f7783fce3
Saadiq's Dev <96834576+Mrsupertag@users.noreply.github.com>
2022-11-03 17:02:47 +0600

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,28 +1,44 @@
-print ("Welcome!!")
-add = "+"
-sub = "-"
-multi = "*"
-div = "/" 
-user_input = input("Enter type: ")
-user_input2 = input("Enter number 1: ")
-user_input3 = input("Enter number 2: ")
+def add(x, y):
+    return x + y
 
-if user_input == add:
-  print(user_input2)
-  print(user_input3)
-  print(user_input2 + user_input3)
-  
-if user_input == sub:
-  print(user_input2)
-  print(user_input3)
-  print(user_input2 - user_input3)
-  
-if user_input == multi:
-  print(user_input2)
-  print(user_input3)
-  print(user_input2 * user_input3)
-  
- if user_input == div:
-   print(user_input2)
-   print(user_input3)
-   print(user_input2 / user_input3)
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    choice = input("Enter choice(1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    
+    else:
+        print("Invalid Input")
